fb0ffbe76d855c84e12006e11248781f90b5f9d7
Ivan Kudrov <Ivankudrov1811@gmail.com>
2023-01-26 21:02:05 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+# 1 Написать программу, запрашивающую имя, фамилию и возраст пользователя и выводящую строку вида:
+#         Hello, <Фамилия пользователя> <Имя пользователя>. Your age is: <возраст>
+import math
+
+# name=input('Enter please your name: ')
+# surname=input('Enter please your surname: ')
+# age = str(input('Enter please your age:'))
+#
+# print("Hello, "+surname+' '+name+'. '+'Your age is: '+age)
+
+2
+# Напишите программу, которая находит длину гипотенузы для прямоугольного треугольника по двум катетам.(с=sqrt(a * a + b * b))
+
+# side1=int(input('Enter firs side: '))
+# side2=int(input('Enter second side: '))
+# g=math.sqrt(side1^2+side2^2)
+# print(g)
+
+# 3 Пользователь вводит длины трех
+# сторон треугольника(a, b, c)[тип float].Напишите программу, которая проверяет, является ли треугольник прямоугольным(с2=a2 + b2)
+# a=float(input('please enter first side: '))
+# b=float(input('please enter second side: '))
+# c=float(input('please enter third side: '))
+#
+# if b * b != c * c and b * b != a * a and c * c != b * b:
+#   print("The triangle is not right-angled.")
+# else:
+#   print("The triangle is right-angled.")
+
+# 4 Пользователь вводит некоторый произвольный список list. Написать программу, выводящую элементы списка в обратном порядке
+# list1 = list(input('Please enter the list: '))
+# list1.reverse()
+# print(list1)
+
+
+# 5 Как известно, кортеж является неизменяемым типом. Напишите программу, которая позволяет в кортеж A добавить кортеж B таким образом, что кортеж B помещается на index[2]
+# tuple1=(1, 2, 3, 5, 8)
+# tuple2=(8,2,5)
+# emp_tuple=(tuple1[0],tuple1[1],tuple2[0],tuple2[1],tuple2[2],tuple1[2],tuple1[3],tuple1[4])
+#
+# print(emp_tuple)
+
+# 6*Написать программу,которая для произвольного списка находит число/числа,наиболее часто встречающееся в данном списке и возвращающее их такжe в виде списка.
+
+list1=[1, 2, 3, 4, 7, 9, 9]
+
+
+
